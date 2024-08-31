f6686186ad61b9ea291175a33cd2e268fb032960
Mayank Gupta <mg919889@gmail.com>
2024-08-31 23:28:21 +0530

    Initialize

---
+++ frontend/notes-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from '../src/pages/Home/Home'
+const App = () => {
+  return (
+    <>
+    <div className='w-full h-screen bg-zinc-800'>
+     <Home></Home>
+    </div>
+    </>
+  )
+}
+
+export default App

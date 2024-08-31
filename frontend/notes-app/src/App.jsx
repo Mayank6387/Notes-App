727ec56bbf950b2efd1356a5a2f7cae810302b74
Mayank Gupta <mg919889@gmail.com>
2024-09-01 00:09:17 +0530

    Initial

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,11 +1,10 @@
-import Home from '../src/pages/Home/Home'
+import Home from "./pages/Home/Home"
+
 const App = () => {
   return (
-    <>
-    <div className='w-full h-screen bg-zinc-800'>
-     <Home></Home>
+    <div className="w-screen h-full bg-gray-800">
+      <Home></Home>
     </div>
-    </>
   )
 }
 

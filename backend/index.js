c3a2d1d1c6abfa19dcd93d5ff4d204c0069eee22
Mayank Gupta <mg919889@gmail.com>
2024-09-14 12:25:24 +0530

    Login

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,45 @@ app.post("/createaccount",async(req,res)=>{
 })
 
 
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+
+    if(!email){
+        return res.status(400).json({message:'Email is Required'})
+    }
+    if(!password){
+        return res.status(400).json({message:'Password is Required'})
+    }
+    try{
+      const userInfo=await userModel.findOne({email:email});
+
+      if(!userInfo){
+        return res.status(404).json({message:"User not Found"})
+      }
+      const isMatch=await bcrypt.compare(password,userInfo.password);
+      if(userInfo.email==email && isMatch){
+         const user={user:userInfo};
+         const accesstoken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"36000m"
+         });
+
+         return res.json({
+            error:false,
+            message:"Login Succesfull",
+            email,
+            accesstoken
+         })
+      }else{
+        return res.status(400).json({
+            error:true,
+            message:"Invalid Credentials"
+        })
+      }
+    }catch(err){
+        console.log(err,"Failed to Login")
+    }
+})
 app.listen(3000)
 
 module.exports=app;
